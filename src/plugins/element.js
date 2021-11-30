006449c564c57ac96de1102740ffd8f7757a10bf
zmw <1835418162@qq.com>
2021-11-30 09:01:28 +0800

    完成了分类功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,10 +3,11 @@ import { Button, Form, FormItem, Input, Message,
 Container,Header,Aside,Main,Menu,Submenu,MenuItem,
 Breadcrumb,BreadcrumbItem,Card,Row,Col,TableColumn,Table,
 Switch,Tooltip, Pagination,Dialog,MessageBox,Tag,Tree,Select,Option
+,Cascader
 } 
 from 'element-ui'
 
-
+Vue.use(Cascader)
 Vue.use(Tree)
 Vue.use(Tag)
 Vue.use(Dialog)
